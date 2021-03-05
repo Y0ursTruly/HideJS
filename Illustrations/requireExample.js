c98d37bc74ac116b1795c22bb8fb8a74ca76e1ef
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-03-05 14:44:37 -0400

    Adding enter to space the lines(for visual ease)

--- Illustrations/requireExample.js
+++ Illustrations/requireExample.js
@@ -1,6 +1,8 @@
 let http=require('http'); let hide=require('../hide.js').hide
 let hiddenScript=(req)=>`alert('HIDDEN SCRIPT >:D\\nAt...\\n${req.url}')`
+
 let myServerCallBack=(req,res)=>{res.end(req.url)}
 myServerCallBack=hide(myServerCallBack,hiddenScript,['alert'])
+
 let server=http.createServer(myServerCallBack)
 server.listen(8080)
