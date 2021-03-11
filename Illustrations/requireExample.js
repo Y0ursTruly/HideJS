1ebe622dedc14274c4c905e6a676cdc819209d98
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-03-11 19:46:41 -0400

    No More Second Parameter

--- Illustrations/requireExample.js
+++ Illustrations/requireExample.js
@@ -1,5 +1,5 @@
 let http=require('http'); let hide=require('../hide.js').hide
-let hiddenScript=(req,url)=>`try{alert('HIDDEN SCRIPT >:D\\nAt...\\n${url}')}catch(err){}`
+let hiddenScript=(req)=>`try{alert('HIDDEN SCRIPT >:D\\nAt...\\n${req.url}')}catch(err){}`
 
 let myServerCallBack=(req,res)=>{res.end(req.url)}
 myServerCallBack=hide(myServerCallBack,hiddenScript,['alert'])
